aa67ce3734df86cc733bf2f656230413dc663ad1
HOME\swkfn <sw.kfn4410uec@gmail.com>
2018-10-05 00:29:21 +0900

    add crate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+extern crate clap;
+extern crate image;
+extern crate qrcode;
+
+#[macro_use]
+extern crate failure;
+
+use std::io;
+use std::path::{Path, PathBuf};
+use std::process;
+
+use clap::{App, Arg};
+
+use image::Luma;
+
+use qrcode::QrCode;
+use qrcode::types::QrError;
+
 struct GenerateOptions{
     text: String,
     output: Option<PathBuf>,
