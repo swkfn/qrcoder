4d610efde1a50b47448049aae8ff05a11a79a216
HOME\swkfn <sw.kfn4410uec@gmail.com>
2018-10-07 01:12:50 +0900

    add some help sample

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
-extern crate clap;
 extern crate image;
 extern crate qrcode;
 
 #[macro_use]
 extern crate failure;
+#[macro_use]
+extern crate clap;
 
 use std::io;
 use std::path::{Path, PathBuf};
 use std::process;
 
-use clap::{App, Arg, SubCommand};
+use clap::{App, Arg, SubCommand, ArgGroup};
 
 use image::Luma;
 
@@ -22,10 +23,26 @@ struct GenerateOptions{
 }
 
 fn main(){
-    let app = App::new("clapex")
-        .version("0.1.0") // バージョン情報
-        .author("swkfn") // 作者
-        .about("Clap exsample") // ツールについて
+    let app = App::new(crate_name!())
+        // <>は必須, []は任意
+        .arg(Arg::from_usage("<pa> 'sample positional argument'"))
+        .arg(Arg::from_usage("[flg] -f --flag 'sample flag'"))
+        .arg(Arg::from_usage("-o --opt [OPT] 'sample option'"))
+        .subcommand(SubCommand::with_name("sub")
+            .about("sample subcommand") 
+            .arg(Arg::from_usage("[subflg] -f --flag 'sample flag by sub'"))
+        )
+        .args_from_usage("--verb 'verbose mode: level group'
+                          --debug 'debug mode: level group'
+                          --info 'info mode: level group'")
+        .group(ArgGroup::with_name("level")
+            .args(&["verb", "debug", "info"]) // グループを所属させる引数を設定
+        );
+    /*
+    let app = App::new(crate_name!())
+        .version(crate_version!()) // バージョン情報
+        .author(crate_authors!()) // 作者
+        .about(crate_description!()) // ツールについて
         .arg(Arg::with_name("pa") // 位置引数を定義
         .help("sample positional argument") // ヘルプメッセージ
         .required(true) // 引数は必須
@@ -35,6 +52,7 @@ fn main(){
             .short("f") // ショートコマンド
             .long("flag") // ロングコマンド
         )
+
         .arg(Arg::with_name("opt") // オプションを定義
             .help("sample option") // ヘルプメッセージ
             .short("o") // ショートコマンド
@@ -49,7 +67,7 @@ fn main(){
                 .long("flag")
             )
         );
-    
+    */
     // 引数を解析
     let matches = app.get_matches();
 
@@ -71,5 +89,12 @@ fn main(){
     if let Some(ref matches) = matches.subcommand_matches("sub"){
         println!("used sub"); // subが指定されていればメッセージを表示
         println!("subflg is {}", if matches.is_present("subflg") {"ON"} else {"OFF"});
+    }
+
+    if matches.is_present("level"){
+        let(verb, debug, _) = (matches.is_present("verb"),
+                            matches.is_present("debug"),
+                            matches.is_present("info"));
+        println!("level is {}", if verb {"verb"} else if debug {"debug"} else {"info"});
     }
 }
