cfa92c8b62b8d7af070b90fea2b23016272546f3
HOME\swkfn <sw.kfn4410uec@gmail.com>
2018-10-07 18:41:15 +0900

    add option

--- src/main.rs
+++ src/main.rs
@@ -22,12 +22,27 @@ struct GenerateOptions{
     output: Option<PathBuf>,
 }
 
-fn main(){
+enum Command{
+    GenerateCode(GenerateOptions),
+}
+
+type AppResult<T> = Result<T, AppError>;
+
+#[derive(Debug, Fail)]
+enum AppError {
+    #[fail(display = "qr code error: {}", err)]
+    QrError{ err: QrError },
+
+    #[fail(display = "io error: {}", err)]
+    IoError{ err: io::Error },
+}
+
+fn parse_command() -> AppResult<Command>{
     let app = App::new(crate_name!())
         // <>は必須, []は任意
-        .arg(Arg::from_usage("<pa> 'sample positional argument'"))
+        .arg(Arg::from_usage("<TEXT> 'QRコードに埋め込む文字列を指定してください.'"))
+        .arg(Arg::from_usage("-o --output [OUTPUT] '出力先ファイルパスを指定してください.'"))
         .arg(Arg::from_usage("[flg] -f --flag 'sample flag'"))
-        .arg(Arg::from_usage("-o --opt [OPT] 'sample option'"))
         .subcommand(SubCommand::with_name("sub")
             .about("sample subcommand") 
             .arg(Arg::from_usage("[subflg] -f --flag 'sample flag by sub'"))
@@ -38,48 +53,12 @@ fn main(){
         .group(ArgGroup::with_name("level")
             .args(&["verb", "debug", "info"]) // グループを所属させる引数を設定
         );
-    /*
-    let app = App::new(crate_name!())
-        .version(crate_version!()) // バージョン情報
-        .author(crate_authors!()) // 作者
-        .about(crate_description!()) // ツールについて
-        .arg(Arg::with_name("pa") // 位置引数を定義
-        .help("sample positional argument") // ヘルプメッセージ
-        .required(true) // 引数は必須
-        )
-        .arg(Arg::with_name("flg") // フラグを定義
-            .help("sample flag") // ヘルプメッセージ
-            .short("f") // ショートコマンド
-            .long("flag") // ロングコマンド
-        )
 
-        .arg(Arg::with_name("opt") // オプションを定義
-            .help("sample option") // ヘルプメッセージ
-            .short("o") // ショートコマンド
-            .long("opt") // ロングコマンド
-            .takes_value(true) // 値を持つことを定義
-        )
-        .subcommand(SubCommand::with_name("sub") // サブコマンドを定義
-            .about("sample subcommand")
-            .arg(Arg::with_name("subfig")
-                .help("sample flag by sub")
-                .short("f")
-                .long("flag")
-            )
-        );
-    */
     // 引数を解析
     let matches = app.get_matches();
-
-    // paが指定されていれば値を表示
-    if let Some(o) = matches.value_of("pa"){
-        println!("Value for pa {}", o);
-    }
-
-    // optが指定されていれば値を表示
-    if let Some(o) = matches.value_of("opt"){
-        println!("Value for opt {}", o);
-    }
+    let text = String::from(matches.value_of("TEXT").unwrap());
+    let output = matches.value_of("OUTPUT").map(PathBuf::from);
+    let generate_options = GenerateOptions{ text, output };
 
     // flgのON/OFFで表示するメッセージを切り替え
     println!("flg is {}", if matches.is_present("flg") {"ON"} else {"OFF"});
@@ -90,11 +69,16 @@ fn main(){
         println!("used sub"); // subが指定されていればメッセージを表示
         println!("subflg is {}", if matches.is_present("subflg") {"ON"} else {"OFF"});
     }
-
+    
     if matches.is_present("level"){
         let(verb, debug, _) = (matches.is_present("verb"),
                             matches.is_present("debug"),
                             matches.is_present("info"));
         println!("level is {}", if verb {"verb"} else if debug {"debug"} else {"info"});
     }
+    Ok(Command::GenerateCode(generate_options))
+}
+
+fn main(){
+    println!("Hello World");
 }
